e92dac6703081420a7a37871b0522fe5d6f7884b
Janusz Dziemidowicz <rraptorr@nails.eu.org>
2014-01-08 14:15:51 +0100

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -10,31 +10,21 @@ major=`echo $version | cut -d" " -f 1`
 minor=`echo $version | cut -d" " -f 2`
 debian=`echo $version | cut -d" " -f 3`
 
-while [ $# -gt 0 ] ; do
-    case "$1" in
-        *UnlimitedJCEPolicyJDK7.zip)
-            rm -rf "$dir"/UnlimitedJCEPolicy
-            unzip -q "$1" -d "$dir"
-            tar -czf "$dir"/../oracle-java${major}_${major}.${minor}.orig-UnlimitedJCEPolicy.tar.gz -C "$dir" UnlimitedJCEPolicy
-            rm -f "$1"
-            ;;
-        *jdk-${major}u${minor}-linux-i586.tar.gz)
-            rm -rf "$dir"/i586 "$dir"/jdk1.${major}.0_${minor}
-            tar xf "$1" -C "$dir"
-            mv "$dir"/jdk1.${major}.0_${minor} "$dir"/i586
-            mv "$1" "$dir"/../oracle-java${major}_${major}.${minor}.orig-i586.tar.gz
-            ;;
-        *jdk-${major}u${minor}-linux-x64.tar.gz)
-            rm -rf "$dir"/x64 "$dir"/jdk1.${major}.0_${minor}
-            tar xf "$1" -C "$dir"
-            mv "$dir"/jdk1.${major}.0_${minor} "$dir"/x64
-            mv "$1" "$dir"/../oracle-java${major}_${major}.${minor}.orig-x64.tar.gz
-            ;;
-        *)
-            echo unknown file: \"$1\"
-            ;;
-    esac
-    shift
-done
+make -f "$dir"/debian/rules -C "$dir" get-orig-source
+
+rm -rf "$dir"/UnlimitedJCEPolicy
+unzip -q "$dir"/UnlimitedJCEPolicyJDK7.zip -d "$dir"
+tar -czf "$dir"/../oracle-java${major}_${major}.${minor}.orig-UnlimitedJCEPolicy.tar.gz -C "$dir" UnlimitedJCEPolicy
+rm -f "$dir"/UnlimitedJCEPolicyJDK7.zip
+
+rm -rf "$dir"/i586 "$dir"/jdk1.${major}.0_${minor}
+tar xf "$dir"/jdk-${major}u${minor}-linux-i586.tar.gz -C "$dir"
+mv "$dir"/jdk1.${major}.0_${minor} "$dir"/i586
+mv "$dir"/jdk-${major}u${minor}-linux-i586.tar.gz "$dir"/../oracle-java${major}_${major}.${minor}.orig-i586.tar.gz
+
+rm -rf "$dir"/x64 "$dir"/jdk1.${major}.0_${minor}
+tar xf "$dir"/jdk-${major}u${minor}-linux-x64.tar.gz -C "$dir"
+mv "$dir"/jdk1.${major}.0_${minor} "$dir"/x64
+mv "$dir"/jdk-${major}u${minor}-linux-x64.tar.gz "$dir"/../oracle-java${major}_${major}.${minor}.orig-x64.tar.gz
 
 git --git-dir="$dir"/.git archive -o "$dir"/../oracle-java${major}_${major}.${minor}.orig.tar.gz --prefix=oracle_java${major}-${major}.${minor}/ HEAD README.md prepare.sh
